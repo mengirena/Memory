24052af5b4a7053f91338d76b89846f24b3e186e
Irena Chen <irena1009@gmail.com>
2021-08-08 17:33:44 -0700

    add provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//initialize redux
 import { Provider } from 'react-redux'; //keep track of global state, store, and make sure it can be used inside anywhere of the app
-import {  } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+
+import reducers from './reducers'
 
 import App from './App';
 
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
